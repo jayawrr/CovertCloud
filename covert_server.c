59adae3c65e8fbbd5970ba8e2daea93653a8ff6f
Michael <shavitmichael@gmail.com>
2015-12-10 09:54:35 +0000

    updated period

--- covert_server.c
+++ covert_server.c
@@ -12,7 +12,7 @@ int main( int argc, char *argv[] )
 
     for( i = 0; i < 100; i++ )
     {
-        covert_write_bit( value ^= 1, 1000000 );
+        covert_write_bit( value ^= 1, 2050000 );
         printf( "%d %d\n", i, value );
    }
 
