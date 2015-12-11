0e3c6495f30c4dfd18cfe2bd7afc2532bd203b8f
Michael Shavit <shavitmichael@gmail.com>
2015-12-11 08:23:55 +0000

    yey, pretty good

--- covert_client.c
+++ covert_client.c
@@ -20,8 +20,20 @@ void gettime(struct timeval *t) {
   }
 }
 
-int series_length(struct timeval *t) {
- 
+// Find out how many periods occur in the duration t:
+time_t series_length(struct timeval *t) {
+// Ideally we would do division between timeval, but let's not bother for this.
+// Ugh, instead for now let's do the following:
+// With a period of 2 seconds, a samp_period of 2.3 seconds, 0bit periods 
+// very rarely go over 3 seconds, and 1 bit periods rarely go under 2 seconds.
+// Let's just round anything over 1 second up to two seconds, and round down
+// anything under 1 seconds.
+  time_t mod = t->tv_sec % PERIOD_SEC;
+  time_t ret = (t->tv_sec - mod) / PERIOD_SEC;
+  if (mod > 1) {
+     ret += 1;
+  }
+  return ret;
 }
 
 // Moves any time that is in excess of a multiplier of the PERIOD from source
@@ -29,14 +41,27 @@ int series_length(struct timeval *t) {
 // This is usefull for measurements which bias towards source.
 // For e.g, the data often shows: period = 2, dest = 1.6, source = 2.4
 // this will set dest = (2.4 - 2) + 1.6 =2 and set source = 2
+// NOTE: this works well for square wave, untested for other signals.
 void balance_timing(struct timeval *dest, struct timeval *source) {
   struct timeval period;
   struct timeval excess;
+  struct timeval temp;
+  struct timeval zero;
+  zero.tv_sec = 0;
+  zero.tv_usec = 0;
   period.tv_sec = PERIOD_SEC;
   period.tv_usec = PERIOD_USEC;
-  timersub(source, &period, &excess); 
-  //TODO don't assume excess is positive. Data shows almost always postive so ok.
-  timeradd(&excess, dest, dest);
+
+  temp.tv_usec = source->tv_usec;
+  temp.tv_sec  = source->tv_sec % (2 * PERIOD_SEC);
+  timersub(&temp, &period, &excess); 
+  
+
+  // Is there any excess?:
+  if (timercmp(&excess, &zero, >)) {
+    timersub(source, &excess, source);
+    timeradd(&excess, dest, dest);
+  }
 }
 
 int main( int argc, char *argv[] )
@@ -44,6 +69,7 @@ int main( int argc, char *argv[] )
   struct timeval  l_start, h_start, h_end, curr, samp_period, l_diff, h_diff;
   struct timeval prevt;
   long            elapsed;
+  int count;
   int prev_bit = 0;
   samp_period.tv_sec = 0;
   samp_period.tv_usec = SAMP_PERIOD;
@@ -55,6 +81,7 @@ int main( int argc, char *argv[] )
 
   int bit = 0;
   int prevbit = 0;
+  time_t length;
   // Signal parsing loop:
   while(1) {
     // Time a read:
@@ -75,11 +102,19 @@ int main( int argc, char *argv[] )
           // We also use this opportunity to print out the series of 1 bits
           // that preceded this series of 0 bits.
           timersub(&h_end, &h_start, &h_diff);
-          balance_timing(&h_diff, &h_diff);
 
+          printf("old_hdiff: %ld\n", h_diff.tv_sec);
+          printf("old_hdiff: %ld\n", h_diff.tv_usec);
+        printf("l_diff: %ld\n", l_diff.tv_sec);
+        printf("l_diff: %ld\n", l_diff.tv_usec);
           
+          balance_timing(&h_diff, &l_diff);
           printf("h_diff: %ld\n", h_diff.tv_sec);
           printf("h_diff: %ld\n", h_diff.tv_usec);
+          length = series_length(&h_diff);
+          for (count = 0; count < length; count++) {
+            printf("1\n");
+          }
         }
         // Reset high_start time
         //gettime(&h_start);
@@ -88,6 +123,10 @@ int main( int argc, char *argv[] )
         // print the series of 0 bits.
         printf("l_diff: %ld\n", l_diff.tv_sec);
         printf("l_diff: %ld\n", l_diff.tv_usec);
+          length = series_length(&l_diff);
+          for (count = 0; count < length; count++) {
+            printf("0\n");
+          }
       } 
     } else if(!bit && prevbit == 1) {
       // Case: falling edge
