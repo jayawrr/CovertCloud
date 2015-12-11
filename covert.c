3dad5a55803fab474fab1d14e5301361f4fbe6ab
Your Name <you@example.com>
2015-12-11 20:04:44 +0000

    cleaned up covert.c

--- covert.c
+++ covert.c
@@ -16,122 +16,17 @@
 #define TIMER           ITIMER_REAL
 #define TIMER_SIGNAL    SIGALRM
 
-#define WRITE_LEN  104857
+#define WRITE_LEN_SEND  1048576
+#define WRITE_LEN_REC  104857
 
 volatile sig_atomic_t flag;
-static char buf[WRITE_LEN];
+//Assumes WRITE_LEN_SEND > WRITE_LEN_REC
+static char buf[WRITE_LEN_SEND];
 
 /**
     Timer interrupt service routine. Raises flag indicating timer reached.
 */
 void bit_timer_isr(int sig) { flag = 1; }
-
-/** 
-    Attempts to write to disk within given threshold of time (useconds).
-    Returns 1 if time taken is longer than threshold, 0 otherwise.
-*/
-int covert_read_bit( long threshold )
-{
-    int                 file;
-    int                 result, num;
-
-    struct itimerval    timer, old_timer, empty_timer;
-    struct sigaction    sa, old_sa;
-
-    /* Clear flag */
-    flag = 0;
-
-    /* Configure interrupt service routine */
-    sa.sa_handler = bit_timer_isr;
-    sa.sa_flags = 0;
-    sigemptyset( &sa.sa_mask );
-    if ( sigaction(TIMER_SIGNAL, &sa, &old_sa) < 0 )
-    {
-        perror( "covert_read_bit: sigaction error" );
-        exit(1);
-    }
-
-    /* Set timer to threshold value */
-    timer.it_interval.tv_sec = 0;
-    timer.it_interval.tv_usec = 0;
-    timer.it_value.tv_sec = threshold / 1000000;
-    timer.it_value.tv_usec = threshold % 1000000;
-    if ( setitimer( TIMER, &timer, &old_timer ) < 0 ) 
-    {
-        perror( "covert_read_bit: setitimer error; setting timer" );
-        exit(1);
-    }
-
-    /* Begin writing to file. If any of these methods are interrupted by the 
-       timer, bit_timer_isr will be called and the flag will be set. */
-    /* Open file for writing, clear any existing content, create if necessary */
-    if ( flag ) goto FINISH_READ;
-    if ( (file = open( WRITE_FILE, O_WRONLY|O_TRUNC|O_CREAT, S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP )) < 0 )
-    {
-        if ( errno == EINTR ) goto FINISH_READ;
-        perror( "covert_read_bit: fopen error" );
-        exit(1);
-    }
-    /* Write to file */
-    num = 0;
-    while ( num < WRITE_LEN )
-    {
-        if ( flag ) goto FINISH_READ;
-        if ( (num += write( file, buf, WRITE_LEN-num )) < 0 )
-        {
-            if ( errno == EINTR ) goto FINISH_READ;
-            perror( "covert_read_bit: fwrite error" );
-            exit(1);
-        }
-    }
-    /* Flush write */
-    if ( flag ) goto FINISH_READ;
-    if ( fsync( file ) < 0 )
-    {
-        if ( errno == EINTR ) goto FINISH_READ;
-        perror( "covert_read_bit: fclose error" );
-        exit(1);
-    }
-
-    if ( flag ) goto FINISH_READ;
-
-    /* Cancel timer */
-    timer.it_interval.tv_sec = 0;
-    timer.it_interval.tv_usec = 0;
-    timer.it_value.tv_sec = 0;
-    timer.it_value.tv_usec = 0;
-    if ( setitimer( TIMER, &timer, NULL ) < 0 ) 
-    {
-        perror( "covert_read_bit: setitimer error; restoring timer" );
-        exit(1);
-    }
-
-
-FINISH_READ:
-    /* Close file */
-    if( close( file ) < 0 )
-    {
-        perror( "covert_read_bit: error closing file" );
-        exit(1);
-    }
-
-    /* Replace old timer and sigaction */
-    if ( sigaction(TIMER_SIGNAL, &old_sa, NULL) < 0 )
-    {
-        perror( "covert_read_bit: sigaction error" );
-        exit(1);
-    }
-    if ( setitimer( TIMER, &old_timer, NULL ) < 0 ) 
-    {
-        perror( "covert_read_bit: setitimer error; " );
-        exit(1);
-    }
-
-    /* Return status of flag */
-    result = flag;
-    return result;
-}
-
 
 
 /**
@@ -161,9 +56,9 @@ long covert_read_time()
     }
     /* Write to file */
     num = 0;
-    while ( num < WRITE_LEN )
+    while ( num < WRITE_LEN_REC )
     {
-        if ( (num += write( file, buf, WRITE_LEN-num )) < 0 )
+        if ( (num += write( file, buf, WRITE_LEN_REC-num )) < 0 )
         {
             perror( "covert_read_time: fwrite error" );
             exit(1);
@@ -256,10 +151,10 @@ void covert_write_bit( int value, long period )
             }
             /* Write to file */
             num = 0;
-            while ( num < WRITE_LEN )
+            while ( num < WRITE_LEN_SEND )
             {
                 if ( flag ) goto FINISH_WRITE;
-                if ( (num += write( file, buf, WRITE_LEN-num )) < 0 )
+                if ( (num += write( file, buf, WRITE_LEN_SEND-num )) < 0 )
                 {
                     if ( errno == EINTR ) goto FINISH_WRITE;
                     perror( "covert_write_bit: fwrite error" );
