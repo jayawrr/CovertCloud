ec8978593d63ce567348c945dd60a176aa06b0c0
Michael Shavit <shavitmichael@gmail.com>
2015-12-11 07:34:49 +0000

    l_diff/h_diff

--- covert_client.c
+++ covert_client.c
@@ -4,9 +4,12 @@
 
 #include "covert.h"
 
-#define THRESHOLD   10000
+#define THRESHOLD   7500
 #define PERIOD 	    2000000
-#define SAMP_PERIOD 1500000
+// length of time of consecutive low reads which mark the end of 1 bits.
+// data has show to be biased to 0s, therefore try to counteract
+// the bias.
+#define SAMP_PERIOD 2300000
 
 void gettime(struct timeval *t) {
   if( gettimeofday( t, NULL ) < 0 )
@@ -22,7 +25,7 @@ int series_length(struct timeval *t) {
 
 int main( int argc, char *argv[] )
 {
-  struct timeval  l_start, h_start, h_end, curr, samp_period, diff;
+  struct timeval  l_start, h_start, h_end, curr, samp_period, l_diff, h_diff;
   struct timeval prevt;
   long            elapsed;
   int prev_bit = 0;
@@ -48,34 +51,33 @@ int main( int argc, char *argv[] )
     gettime(&curr);
     if(bit && prevbit == 0) {
       // Case: rising edge
-      timersub(&curr, &l_start, &diff); 
-      if(timercmp(&diff, &samp_period, >)) {
+      timersub(&curr, &l_start, &l_diff); 
+      if(timercmp(&l_diff, &samp_period, >)) {
         // The rising edge end signals the end of a series of 0 bits.
         
         if(timercmp(&h_end, &h_start, !=)) {
           // We also use this opportunity to print out the series of 1 bits
           // that preceded this series of 0 bits.
-          timersub(&h_end, &h_start, &diff);
-          printf("h_diff: %ld\n", diff.tv_sec);
-          printf("h_diff: %ld\n", diff.tv_usec);
+          timersub(&h_end, &h_start, &h_diff);
+          printf("h_diff: %ld\n", h_diff.tv_sec);
+          printf("h_diff: %ld\n", h_diff.tv_usec);
         }
         // Reset high_start time
         //gettime(&h_start);
         // set it to prevt to not ignore the time of the current read. 
         h_start = prevt;
         // print the series of 0 bits.
-        timersub(&curr, &l_start, &diff); 
-        printf("l_diff: %ld\n", diff.tv_sec);
-        printf("l_diff: %ld\n", diff.tv_usec);
+        printf("l_diff: %ld\n", l_diff.tv_sec);
+        printf("l_diff: %ld\n", l_diff.tv_usec);
       } 
     } else if(!bit && prevbit == 1) {
       // Case: falling edge
       h_end = curr;
       l_start = curr;
     }
-    usleep(10000); // To prevent overburdenning the channel from this end.
     prevbit = bit;
     gettime(&prevt);
+    usleep(10000); // To prevent overburdenning the channel from this end.
   } //end while
   return 0;
 }
